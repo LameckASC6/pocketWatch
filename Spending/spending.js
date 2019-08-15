1fef93a9e17e010d5752c47f89e067f491ed48d6
LameckASC6 <lnbane.uachs@gmail.com>
2019-08-14 22:26:37 -0400

    spending page now stays the same even if reloaded

--- Spending/spending.js
+++ Spending/spending.js
@@ -9,17 +9,21 @@ let reciepts = document.getElementById("reciepts");
 let labels;
 let datasets;
 let colors;
+let categoryVal;
+let total = 0;
+let user = sessionStorage.getItem('user');
+totalText.innerText = `Total: $${total}`;
+drawChart();
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
-let total = 0;
 let chartData = {
     type: 'pie',
     data: {
@@ -50,21 +54,15 @@ function inArray(looking, array) {
     return false;
 }
 
-
-let user = sessionStorage.getItem('user');
-totalText.innerText = `Total: $${total}`;
-
-drawChart();
-
 async function addClick(event) {
-    let categoryVal = category.value;
+    categoryVal = category.value;
     categoryVal = categoryVal.charAt(0).toUpperCase() + categoryVal.slice(1).toLowerCase();
     event.preventDefault();
     total = total + parseInt(amount.value);
     totalText.innerText = `Total: $${total}`;
     spendingChart.update();
     let reciept = document.createElement("h4");
-    reciept.id="reciept";
+    reciept.id = "reciept";
     reciept.style.backgroundColor = "#edf4ed";
     reciept.style.marginLeft = "1%";
     reciept.style.marginRight = "1%";
@@ -75,12 +73,15 @@ async function addClick(event) {
     reciepts.prepend(reciept);
     console.log(reciept.innerText);
     console.log(total);
-    console.log(labels, datasets, colors);
     if (categoryVal != "" && amount.value != "" && inArray(categoryVal, chartData.data.labels) != true) {
         chartData.data.labels.push(categoryVal);
         chartData.data.datasets[0].data.push(parseInt(amount.value));
         chartData.data.datasets[0].backgroundColor.push(getRandomColor());
-        
+
+        //Pushing to Database
+        pushChartData();
+        //End of pushing to database
+
     } else if (categoryVal != "" && amount.value != "") {
         let newAmount = chartData.data.datasets[0].data[duplicate] += parseInt(amount.value);
         chartData.data.datasets[0].data.splice([duplicate], 1, newAmount);
@@ -95,13 +96,25 @@ async function drawChart(labels, datasets, colors) {
     labels = labels.val();
     datasets = datasets.val();
     colors = colors.val();
-    for (i = 0; i < labels.length; i++){
+    console.log(labels, datasets, colors)
+
+    labels = Object.keys(labels).map(function (key) {
+        return [labels[key]];
+    });
+    datasets = Object.keys(datasets).map(function (key) {
+        return [datasets[key]];
+    });
+    colors = Object.keys(colors).map(function (key) {
+        return [colors[key]];
+    });
+
+    for (i = 0; i < labels.length; i++) {
         chartData.data.labels.push(labels[i]);
     }
-    for (i = 0; i < datasets.length; i++){
+    for (i = 0; i < datasets.length; i++) {
         chartData.data.datasets[0].data.push(datasets[i]);
     }
-    for (i = 0; i < colors.length; i++){
+    for (i = 0; i < colors.length; i++) {
         chartData.data.datasets[0].backgroundColor.push(colors[i]);
     }
 }
@@ -110,16 +123,26 @@ addButton.addEventListener('click', addClick);
 
 let spendingChart = new Chart(pieChart, chartData);
 
-async function login(e){
-  e.preventDefault();
-  const labels = await database.child(`users/${user}/chartData/labels`).once('value');
-  const datasets = await database.child(`users/${user}/chartData/datasets`).once('value');
-  const colors = await database.child(`users/${user}/chartData/color`).once('value');
+async function login(e) {
+    e.preventDefault();
+    const labels = await database.child(`users/${user}/chartData/labels`).once('value');
+    const datasets = await database.child(`users/${user}/chartData/datasets`).once('value');
+    const colors = await database.child(`users/${user}/chartData/color`).once('value');
 
-  console.log(labels);
-  console.log(datasets);
-  console.log(colors);
+    console.log(labels);
+    console.log(datasets);
+    console.log(colors);
 }
 
+async function pushChartData(labels, datasets, colors) {
+    labels = await database.child(`users/${user}/chartData/labels`);
+    datasets = await database.child(`users/${user}/chartData/datasets`);
+    colors = await database.child(`users/${user}/chartData/color`);
+
+    labels = labels.push(categoryVal);
+    datasets = datasets.push(parseInt(amount.value));
+    colors = colors.push(getRandomColor());
+
+}
 
 
